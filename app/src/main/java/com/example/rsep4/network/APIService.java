e6c7c0a64aff097913b1663235587b0615ccfc66
NoHop3 <u_sux@abv.bg>
2022-07-07 22:46:19 +0200

    Small changes

--- app/src/main/java/com/example/rsep4/network/APIService.java
+++ app/src/main/java/com/example/rsep4/network/APIService.java
@@ -5,10 +5,28 @@ import com.example.rsep4.models.WeatherModel;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface APIService {
 
     @GET("/api/v1/weather")
     Call<List<WeatherModel>> getWeatherList();
+
+    @GET("/api/v1/weather/{city}")
+    Call<WeatherModel> getWeatherForCity(@Path("city") String city);
+
+    @POST("/api/v1/weather")
+    Call<WeatherModel> addWeather(@Body WeatherModel weather);
+
+    @PUT("/api/v1/weather/{city}")
+    Call<WeatherModel> updateWeatherForCity(@Path("city") String city, @Body WeatherModel weather);
+
+    @DELETE("/api/v1/weather/{city}")
+    Call<WeatherModel> deleteWeatherForCity(@Path("city") String city);
 }
